f6461d09a9259ad1134b9d93ff51421b67813448
Anurag1096 <anuragqwerty09@outlook.com>
2025-09-27 12:44:19 +0530

    wip: adding modal comman  component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,45 @@
+import Styles from "@/components/Modal/Modal.module.css";
+import { useEffect } from "react";
+
+
+
+interface ModalState {
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const Modal = ({ children, isOpen, onClose }: ModalState) => {
+  useEffect(() => {
+    const handleEscPress = (e: KeyboardEvent) => {
+      if (e.key === "Escape") onClose();
+    };
+    document.addEventListener("keypress", handleEscPress);
+    return () => document.removeEventListener("keypress", handleEscPress);
+  }, [onClose]);
+
+  if (!isOpen) return null;
+  return (
+    <div onClick={onClose} className={Styles["modal--wrapper"]}>
+      <div
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="modal-title"
+        onClick={(e) => e.stopPropagation()}
+        className={Styles["modal--box"]}
+      >
+        {children}
+        {/* a closing icon */}
+        <button
+          onClick={onClose}
+          aria-label="Close modal"
+          className={Styles["modal--closeButton"]}
+        >
+          &times
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
