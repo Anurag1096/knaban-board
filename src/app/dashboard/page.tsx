95ca8ac00cc46ed97eb65be5c3abb7b990166a5c
Anurag1096 <anuragqwerty09@outlook.com>
2025-12-12 20:37:40 +0530

    feat(kanban board): adding create board

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,37 @@
-'use client'
+"use client";
 import BoardList from "@/components/CreateBoard/BoardList";
+import { useState } from "react";
+import AddBoard from "@/components/CreateBoard/CreateBoard";
 import { useAppSelector } from "@/data/store/hooks";
 //will contain board List
 // component.
 
 export default function DashboardPage() {
-  const BoardArr=useAppSelector(state=> state.board)
+  const [open, setOpen] = useState<boolean>(false);
+  const [boardData, setBoardData] = useState({ name: "", column: "" });
+  const BoardArr = useAppSelector((state) => state.board);
+  const handleAddBoardModal = () => {
+    setOpen(!open);
+  };
+  const handleValUpdate = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setBoardData((prev) => ({ ...prev, [name]: value }));
+  };
   return (
-    <div>
-      <BoardList Boards={BoardArr} />
-    </div>
+    <>
+      <AddBoard
+        isOpen={open}
+        onClose={handleAddBoardModal}
+        onValChange={handleValUpdate}
+        name={boardData.name}
+        column={boardData.column}
+      />
+      <div onClick={handleAddBoardModal}>++</div>
+      <div>
+        <BoardList Boards={BoardArr} />
+      </div>
+    </>
   );
 }
--- src/components/CreateBoard/CreateBoard.tsx
+++ src/components/CreateBoard/CreateBoard.tsx
@@ -2,12 +2,17 @@
 
 import { createBoard } from "@/lib/createBoard";
 import Modal from "../Modal/Modal";
+import { InputBox } from "../InputBox/InputBox";
+import { Button } from "../Button/Button";
 
 interface Props {
+  name:string;
+  column:string;
   isOpen: boolean;
   onClose: () => void;
+  onValChange:(e:React.ChangeEvent<HTMLInputElement | HTMLSelectElement>)=>void;
 }
-export default function AddBoard({ isOpen, onClose }: Props) {
+export default function AddBoard({ isOpen, onClose,name,column ,onValChange}: Props) {
   function AddNewBoard() {
     const Board = createBoard({ name: "task" });
     console.log(Board);
@@ -17,12 +22,14 @@ export default function AddBoard({ isOpen, onClose }: Props) {
     <>
       <Modal isOpen={isOpen} onClose={onClose}>
         <div>
-          <h4>
-            This file will be an modal which handle the creation of the kanban
-            board
-          </h4>
+         <form onSubmit={AddNewBoard}>
+          <label htmlFor="title">Name</label>
+          <InputBox variant="text" id="title" inputName="title" value={name} handleChagne={onValChange} required={true} readonly={false}/>
+         <label htmlFor="column"> Columns</label>
+         <InputBox variant="number" id="column" inputName="column" value={column} handleChagne={onValChange} required={true} readonly={false} />
+          <Button type="submit">Add Board</Button>
+         </form>
 
-          <button onClick={AddNewBoard}>Add Board</button>
         </div>
       </Modal>
     </>
