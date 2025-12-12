6ecaf6b9361d833607bf05cdeefc61765fd7fc27
Anurag1096 <anuragqwerty09@outlook.com>
2025-12-12 19:02:36 +0530

    feat(kanban board): adding board creation setup modal

--- src/components/CreateBoard/CreateBoard.tsx
+++ src/components/CreateBoard/CreateBoard.tsx
@@ -1,21 +1,30 @@
-'use client'
+"use client";
 
-import {createBoard} from "@/lib/createBoard";
+import { createBoard } from "@/lib/createBoard";
+import Modal from "../Modal/Modal";
 
-
-export default function AddBoard(){
-    
-    function AddNewBoard(){
-       const Board=createBoard({name:'task'})
-       console.log(Board)
-    }
-    
-    return(<>
-      <div><h4>This file will be an modal which handle the creation of the kanban board</h4>
-      
-      <button onClick={AddNewBoard}>Add Board</button>
-      </div>
-    </>)
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
 }
+export default function AddBoard({ isOpen, onClose }: Props) {
+  function AddNewBoard() {
+    const Board = createBoard({ name: "task" });
+    console.log(Board);
+  }
 
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <div>
+          <h4>
+            This file will be an modal which handle the creation of the kanban
+            board
+          </h4>
 
+          <button onClick={AddNewBoard}>Add Board</button>
+        </div>
+      </Modal>
+    </>
+  );
+}
